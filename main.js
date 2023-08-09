8d5e23673d67b811fa75529ff304bb1fdad4c14a
“David <davidcuentas@gmail.com>
2023-08-08 22:05:30 -0500

    primer comit

---
+++ main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        532:{
+            items:2
+        },
+        746:{
+            items:3
+        },
+        1042:{
+            items:4
+        },
+        1368:{
+            items:5
+        },
+    }
+})
